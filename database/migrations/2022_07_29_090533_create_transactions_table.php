9e0a8f7d61cab84e841ee75b4663177d352637a7
MarnelleApat <marnelle.apat@bible.org.sg>
2022-07-29 17:14:16 +0800

    add transactions group MMC

---
+++ database/migrations/2022_07_29_090533_create_transactions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('transaction_type_id');
+            $table->text('trx_main_data');
+            $table->string('extra1')->nullable();
+            $table->string('extra2')->nullable();
+            $table->string('extra3')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
